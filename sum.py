c5c068db8e7686a56f80ee73308010005de7ac98
JEFFERSON CHAVEZ DIAZ <136255274+PC-Techdeveloper@users.noreply.github.com>
2024-07-16 21:24:46 -0500

    Function sum

---
+++ sum.py
@@ -0,0 +1,10 @@
+print("*** Bienvenido a la suma de dos números 🎉 ***")
+num1 = int(input("Introduce el primer numero: "))
+num2 = int(input("Introduce el segundo numero: "))
+
+
+def sum(num1, num2):
+    return num1 + num2
+
+
+print(f"La respuesta es: {sum(num1, num2)}")
