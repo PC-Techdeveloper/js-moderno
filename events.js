23e2f70fb65510f48a383ffa82fdcc540fdc9f8a
JEFFERSON CHAVEZ DIAZ <136255274+PC-Techdeveloper@users.noreply.github.com>
2024-07-16 20:38:16 -0500

    In process

--- events.js
+++ events.js
@@ -3,22 +3,3 @@ EVENTOS: Los eventos son una forma de interactuar con el navegador, y son una fo
 */
 
 //Organizando la funcionalidad del evento
-
-//Recursión (Patrón de programación) y pila
-//Pensamiento recursivo -> Simplificar la tarea a resolver
-function pow(x, n) {
-  if (n === 1) {
-    return x
-  } else {
-    return x * pow(x, n - 1)
-  }
-}
-
-console.log(pow(2, 3))
-
-//Una recursión suele ser más corta que una iteración -> Operador ternario -> ? :
-function secondPow(x, n) {
-  return n === 1 ? x : secondPow(x * x, n - 1)
-}
-
-console.log(secondPow(2, 3))
