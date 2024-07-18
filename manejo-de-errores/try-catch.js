8667dae06b954b78b62afda6289da1d4cf1f4834
JEFFERSON CHAVEZ DIAZ <136255274+PC-Techdeveloper@users.noreply.github.com>
2024-07-18 17:24:42 -0500

    try...catch finally thro new Error

---
+++ manejo-de-errores/try-catch.js
@@ -0,0 +1,67 @@
+/*
+Try...catch Permite atrapar errores para que el script pueda en luegar de morir, pueda hacer algo más razonable.
+*/
+try {
+  console.error('Inicio de intentos de ejecución')
+  console.error('Fin de la ejecución de try')
+  werwerwrew
+} catch (err) {
+  console.error('¡Un error ha ocurrido! ❌')
+}
+
+let json = '{json malo}'
+
+try {
+  let user = JSON.parse(json)
+  console.log(user.name)
+} catch (err) {
+  console.error('Nuestras disculpas, los datos tienen errores, intentaremos solicitarlos una vez más ❌')
+  console.log(err.name) //Error name
+  console.log(err.message) //Error message
+}
+
+//EL OPERADOR THROW -> Genera un error
+
+/*
+Constructores integrados para manejar errores:
+
+Error, SyntaxError, ReferenceError, TypeError, RangeError, URIError, EvalError, InternalError.
+
+PROPIEDADES DE ERRORES:
+- name -> Nombre del error
+- message -> Argumento del mensaje
+*/
+
+let error1 = new Error('Estas cosas pasan... o_0')
+console.log(error1.name)
+console.log(error1.message)
+
+try {
+  JSON.parse('{json malo o_0}')
+} catch (err) {
+  console.error(err.name)
+  console.error(err.message)
+}
+
+let secondJson = '{"age": 30}'
+
+try {
+  let user = JSON.parse(secondJson)
+  if (!user.name) {
+    throw new SyntaxError('Dato incompleto: Sin nombre!')
+  }
+  console.log(user.name)
+} catch (err) {
+  console.error(`Èrror en JSON: ${err.message}`)
+}
+
+//Try...catch...finally
+
+try {
+  console.log('intenta (try)')
+  if (confirm('¿Cometer un error?')) BAD_CODE()
+} catch (err) {
+  console.log('atrapa (catch)')
+} finally {
+  console.log('finalmente (finally)')
+}
