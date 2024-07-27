4c39156ede5467f3631639b04f4cd1d20d2731aa
JEFFERSON CHAVEZ DIAZ <136255274+PC-Techdeveloper@users.noreply.github.com>
2024-07-26 21:07:21 -0500

    learning js

--- eventos-ui/moviendo-el-mouse.js
+++ eventos-ui/moviendo-el-mouse.js
@@ -1,1 +1,37 @@
-//Moviendo el mouse: mouseover/out, mouseenter/leave -> 🟡
+//Moviendo el mouse: mouseover/out, mouseenter/leave
+
+/*
+Para mouseover:
+- event.target -> Es el elemento al que se acerca el mouse.
+- event.relatedTarget -> Es el elemento de donde proviene el mouse.
+
+Para mouseout:
+- event.target -> Es el elemento que el mouse dejó.
+- event.relatedTarget -> Es el nuevo elemento bajo el cursor por el cual el cursor dejó al anterior.
+*/
+
+container.onmouseover = container.onmouseout = handler;
+
+function handler(event) {
+  function str(el) {
+    if (!el) return 'null';
+    return el.className || el.tagName;
+  }
+
+  log.value +=
+    event.type +
+    ':  ' +
+    'target=' +
+    str(event.target) +
+    ',  relatedTarget=' +
+    str(event.relatedTarget) +
+    '\n';
+  log.scrollTop = log.scrollHeight;
+
+  if (event.type == 'mouseover') {
+    event.target.style.background = 'pink';
+  }
+  if (event.type == 'mouseout') {
+    event.target.style.background = '';
+  }
+}
